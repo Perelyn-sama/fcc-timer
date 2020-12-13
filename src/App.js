d3cf837a0c0537663518adfb5728e109fd522ded
Perelyn-sama <pisckid344@gmail.com>
2020-12-13 11:40:15 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import "./style.css";
 
 export default function App() {
-  const [breakLength, setbreakLength] = useState(5);
-  const [sessionLength, setsessionLength] = useState(25);
+  let [breakLength, setbreakLength] = useState(5);
+  let [sessionLength, setsessionLength] = useState(25);
 
   const startingMinutes = sessionLength;
   let time = startingMinutes * 60;
 
-  const countdownEl = document.getElementById("countdown");
+  const timeLeft = document.getElementById("time-left");
 
   if (breakLength === 5) {
     setInterval(updateCountdown, 1000);
@@ -30,23 +30,69 @@ export default function App() {
 
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    // countdownEl.innerHTML = `${minutes} ${seconds}`;
+    // timeLeft.innerHTML = `${minutes} ${seconds}`;
     time--;
   }
-  console.log(countdownEl);
+  console.log(timeLeft);
+
+  const breakIncrement = () =>
+    breakLength < 5 ? setbreakLength(breakLength + 1) : breakLength;
+
+  const breakDecrement = () =>
+    breakLength > 1 ? setbreakLength(breakLength - 1) : breakLength;
+
+  const sessionIncrement = () => setsessionLength(sessionLength + 1);
+
+  const sessionDecrement = () =>
+    sessionLength > 1 ? setsessionLength(sessionLength - 1) : sessionLength;
+
+  const reset = () => {
+    setbreakLength(5);
+    setsessionLength(25);
+  };
 
   return (
     <div className="App">
       <div id="break-label"> Break label </div>
-      <div id="session-label"> Session label </div>
-      <button id="break-decrement">&#8595;</button>
-      <button id="session-decrement">&#8595;</button>
-      <button id="break-decrement">&#8593;</button>
-      <button id="session-decrement">&#8593;</button>
+      <button
+        onClick={breakIncrement}
+        className="arrow-btn"
+        id="break-increment"
+      >
+        &#8593;
+      </button>
       <div id="break-length"> {breakLength} </div>
+      <button
+        onClick={breakDecrement}
+        className="arrow-btn"
+        id="break-decrement"
+      >
+        &#8595;
+      </button>
+      <br />
+      <div id="session-label"> Session label </div>
+      <button
+        onClick={sessionIncrement}
+        className="arrow-btn"
+        id="session-increment"
+      >
+        &#8593;
+      </button>
       <div id="session-length"> {sessionLength} </div>
+      <button
+        onClick={sessionDecrement}
+        className="arrow-btn"
+        id="session-decrement"
+      >
+        &#8595;
+      </button>
       <div id="timer-label"> session </div>
-      <div id="countdown"> countdown </div>
+      <div id="time-left"> {sessionLength}:00 </div>
+      <button id="start-stop"> Start / Stop </button>
+      <button onClick={reset} id="reset">
+        {" "}
+        reset{" "}
+      </button>
     </div>
   );
 }
