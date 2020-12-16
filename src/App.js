8e149fe94043304a0bbab12667ff39452ac0cbc7
Perelyn-sama <pisckid344@gmail.com>
2020-12-16 16:42:04 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App() {
     setbreakLength(5);
     setsessionLength(25);
   };
-
+  console.log(<Timer.Seconds />);
   return (
     <div className="App">
       <div id="break-label"> Break length </div>
@@ -58,40 +58,43 @@ export default function App() {
         &#8595;
       </button>
       <div id="timer-label"> session </div>
-      <div id="time-left"> {sessionLength}:00 </div>
-      <button  id="start_stop">
-        {" "}
-        Start / Stop{" "}
-      </button>
-      <button onClick={reset} id="reset">
-        {" "}
-        reset{" "}
-      </button>
-
-      <Timer initialTime={miliSec} direction="backward" startImmediately={false}>
-        {({ start, resume, pause, stop, reset, timerState }) => (
+      <Timer
+        initialTime={miliSec}
+        direction="backward"
+        startImmediately={false}
+      >
+        {({ start, resume, pause, stop, reset }) => (
           <React.Fragment>
-            <Timer.Days /> days
-            <br />
-            <Timer.Hours /> hours
-            <br />
-            <Timer.Minutes /> minutes
-            <br />
-            <Timer.Seconds /> seconds
-            <br />
-            <Timer.Milliseconds /> milliseconds
-            <div>{timerState}</div>
-            <br />
-            <div>
-              <button onClick={start}>Start</button>
-
-              <button onClick={pause}>Pause</button>
-
-              <button onClick={resume}>Resume</button>
-
-              <button onClick={stop}>Stop</button>
-
-              <button onClick={reset}>Reset</button>
+            <div id="time-left">
+              <Timer.Minutes />:
+              <Timer.Seconds
+                formatValue={text =>
+                  text.toString().length > 1 ? text : "0" + text
+                }
+              />
+            </div>
+            <div id="start_stop">
+              <button id="reset" onClick={start}>
+                Start
+              </button>
+              <button id="reset" onClick={pause}>
+                Pause
+              </button>
+              <button id="reset" onClick={resume}>
+                Resume
+              </button>
+              <button id="reset" onClick={stop}>
+                Stop
+              </button>
+              <button
+                id="reset"
+                onClick={() => {
+                  reset();
+                  stop();
+                }}
+              >
+                Reset
+              </button>
             </div>
           </React.Fragment>
         )}
