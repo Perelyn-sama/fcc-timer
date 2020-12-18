e6b01387eb1664595119b7e5584ac949d9654723
Perelyn-sama <pisckid344@gmail.com>
2020-12-18 05:54:17 +0100

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import Timer from "react-compound-timer";
+import Countdown from "react-countdown";
 
 export default function App() {
   let [breakLength, setbreakLength] = useState(5);
   let [sessionLength, setsessionLength] = useState(25);
   let [miliSec, setmilisec] = useState(sessionLength * 60 * 1000);
+  const [date, setDate] = useState(Date.now() + miliSec);
 
   const breakIncrement = () => setbreakLength(breakLength + 1);
 
@@ -17,89 +19,70 @@ export default function App() {
   const sessionDecrement = () =>
     sessionLength > 1 ? setsessionLength(sessionLength - 1) : sessionLength;
 
-  const defReset = () => {
-    setbreakLength(5);
-    setsessionLength(25);
+  const reset = () => setDate(Date.now() + miliSec);
+
+  const renderer = ({ api, formatted }) => {
+    const { minutes, seconds } = formatted;
+    const completed = api.isCompleted();
+    return (
+      <div>
+        <div id="break-label"> Break length </div>
+        <button
+          onClick={breakIncrement}
+          className="arrow-btn"
+          id="break-increment"
+        >
+          &#8593;
+        </button>
+        <p id="break-length"> {breakLength} </p>
+        <button
+          onClick={breakDecrement}
+          className="arrow-btn"
+          id="break-decrement"
+        >
+          &#8595;
+        </button>
+        <br />
+        <div id="session-label"> Session length </div>
+        <button
+          onClick={sessionIncrement}
+          className="arrow-btn"
+          id="session-increment"
+        >
+          &#8593;
+        </button>
+        <div id="session-length"> {sessionLength} </div>
+        <button
+          onClick={sessionDecrement}
+          className="arrow-btn"
+          id="session-decrement"
+        >
+          &#8595;
+        </button>
+        <div id="timer-label"> session </div>
+        <div id="time-left">
+          {" "}
+          {minutes}:{seconds}{" "}
+        </div>
+        <button id="start_stop" onClick={api.start}>
+          Start
+        </button>
+        <button
+          id="reset"
+          onClick={() => {
+            reset();
+            api.stop();
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    );
   };
-  console.log(miliSec);
+
   return (
     <div className="App">
-      <div id="break-label"> Break length </div>
-      <button
-        onClick={breakIncrement}
-        className="arrow-btn"
-        id="break-increment"
-      >
-        &#8593;
-      </button>
-      <p id="break-length"> {breakLength} </p>
-      <button
-        onClick={breakDecrement}
-        className="arrow-btn"
-        id="break-decrement"
-      >
-        &#8595;
-      </button>
-      <br />
-      <div id="session-label"> Session length </div>
-      <button
-        onClick={sessionIncrement}
-        className="arrow-btn"
-        id="session-increment"
-      >
-        &#8593;
-      </button>
-      <div id="session-length"> {sessionLength} </div>
-      <button
-        onClick={sessionDecrement}
-        className="arrow-btn"
-        id="session-decrement"
-      >
-        &#8595;
-      </button>
-      <div id="timer-label"> session </div>
-      <Timer
-        initialTime={miliSec}
-        direction="backward"
-        startImmediately={false}
-      >
-        {({ start, resume, pause, stop, reset }) => (
-          <React.Fragment>
-            <div id="time-left">
-              <Timer.Minutes />:
-              <Timer.Seconds
-                formatValue={text =>
-                  text.toString().length > 1 ? text : "0" + text
-                }
-              />
-            </div>
-            <div id="start_stop">
-              <button id="reset" onClick={start}>
-                Start
-              </button>
-              <button id="reset" onClick={pause}>
-                Pause
-              </button>
-              <button id="reset" onClick={resume}>
-                Resume
-              </button>
-              <button id="reset" onClick={stop}>
-                Stop
-              </button>
-              <button
-                id="reset"
-                onClick={() => {
-                  reset();
-                  stop();
-                  defReset();
-                }}
-              >
-                Reset
-              </button>
-            </div>
-          </React.Fragment>
-        )}
-      </Timer>
+      <Countdown autoStart={false} date={date} renderer={renderer} />
     </div>
   );
 }
