c10ce5170184c6cca4d3a6f9ac8b25af947326e1
Perelyn-sama <pisckid344@gmail.com>
2021-01-05 19:55:09 +0100

    Back to normality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
-import Timer from "react-compound-timer";
-import Countdown from "react-countdown";
 
 export default function App() {
   let [breakLength, setbreakLength] = useState(5);
   let [sessionLength, setsessionLength] = useState(25);
+  let [sec, setSec] = useState("00");
   let [miliSec, setmilisec] = useState(sessionLength * 60 * 1000);
-  const [date, setDate] = useState(Date.now() + miliSec);
 
   const breakIncrement = () => setbreakLength(breakLength + 1);
 
@@ -19,75 +17,69 @@ export default function App() {
   const sessionDecrement = () =>
     sessionLength > 1 ? setsessionLength(sessionLength - 1) : sessionLength;
 
-
   const reset = () => {
-    setDate(Date.now() + miliSec);
     setbreakLength(5);
     setsessionLength(25);
   };
 
-  const renderer = ({ api, formatted }) => {
-    const { minutes, seconds } = formatted;
-    const completed = api.isCompleted();
-    return (
-      <div>
-        <div id="break-label"> Break length </div>
-        <button
-          onClick={breakIncrement}
-          className="arrow-btn"
-          id="break-increment"
-        >
-          &#8593;
-        </button>
-        <p id="break-length"> {breakLength} </p>
-        <button
-          onClick={breakDecrement}
-          className="arrow-btn"
-          id="break-decrement"
-        >
-          &#8595;
-        </button>
-        <br />
-        <div id="session-label"> Session length </div>
-        <button
-          onClick={sessionIncrement}
-          className="arrow-btn"
-          id="session-increment"
-        >
-          &#8593;
-        </button>
-        <div id="session-length"> {sessionLength} </div>
-        <button
-          onClick={sessionDecrement}
-          className="arrow-btn"
-          id="session-decrement"
-        >
-          &#8595;
-        </button>
-        <div id="timer-label"> session </div>
-        <div id="time-left">
-          {" "}
-          {minutes}:{seconds}{" "}
-        </div>
-        <button id="start_stop" onClick={api.start}>
-          Start
-        </button>
-        <button
-          id="reset"
-          onClick={() => {
-            reset();
-            api.stop();
-          }}
-        >
-          Reset
-        </button>
-      </div>
-    );
+  useEffect(() => {
+    // const intervalId = setInterval(startStop, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const startStop = () => {
+    const timeleft = miliSec % 60;
+    miliSec--;
+    setSec(timeleft);
   };
+  console.log(miliSec);
 
   return (
     <div className="App">
-      <Countdown autoStart={false} date={date} renderer={renderer} />
+      <div id="break-label"> Break length </div>
+      <button
+        onClick={breakIncrement}
+        className="arrow-btn"
+        id="break-increment"
+      >
+        &#8593;
+      </button>
+      <p id="break-length"> {breakLength} </p>
+      <button
+        onClick={breakDecrement}
+        className="arrow-btn"
+        id="break-decrement"
+      >
+        &#8595;
+      </button>
+      <br />
+      <div id="session-label"> Session length </div>
+      <button
+        onClick={sessionIncrement}
+        className="arrow-btn"
+        id="session-increment"
+      >
+        &#8593;
+      </button>
+      <div id="session-length"> {sessionLength} </div>
+      <button
+        onClick={sessionDecrement}
+        className="arrow-btn"
+        id="session-decrement"
+      >
+        &#8595;
+      </button>
+      <div id="timer-label"> session </div>
+      <div id="time-left">
+        {" "}
+        {sessionLength}:{sec === 0 ? sec + "0" : sec}{" "}
+      </div>
+      <button onClick={startStop} id="start_stop">
+        Start
+      </button>
+      <button onClick={reset} id="reset">
+        Reset
+      </button>
     </div>
   );
 }
