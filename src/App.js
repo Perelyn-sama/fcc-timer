df7d40c88dd995a2d0411e648922609c998a4912
Perelyn-sama <pisckid344@gmail.com>
2021-01-05 20:33:29 +0100

    Progress

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./style.css";
 export default function App() {
   let [breakLength, setbreakLength] = useState(5);
   let [sessionLength, setsessionLength] = useState(25);
-  let [sec, setSec] = useState("00");
+  let [sec, setSec] = useState(0);
+  let [mins, setMins] = useState(sessionLength);
   let [miliSec, setmilisec] = useState(sessionLength * 60 * 1000);
 
   const breakIncrement = () => setbreakLength(breakLength + 1);
@@ -12,7 +13,10 @@ export default function App() {
   const breakDecrement = () =>
     breakLength > 1 ? setbreakLength(breakLength - 1) : breakLength;
 
-  const sessionIncrement = () => setsessionLength(sessionLength + 1);
+  const sessionIncrement = () => {
+    setsessionLength(sessionLength + 1);
+    setMins(sessionLength + 1);
+  };
 
   const sessionDecrement = () =>
     sessionLength > 1 ? setsessionLength(sessionLength - 1) : sessionLength;
@@ -20,17 +24,23 @@ export default function App() {
   const reset = () => {
     setbreakLength(5);
     setsessionLength(25);
+    setMins(25);
   };
 
-  useEffect(() => {
-    // const intervalId = setInterval(startStop, 1000);
-    return () => clearInterval(intervalId);
-  }, []);
+  // useEffect(() => {
+  // const intervalId = setInterval(startStop, 1000);
+  // return () => clearInterval(intervalId);
+  // }, []);
 
   const startStop = () => {
-    const timeleft = miliSec % 60;
-    miliSec--;
-    setSec(timeleft);
+    const innerFunc = () => {
+      let timeleft = miliSec % 60;
+      miliSec--;
+      timeleft === 59 ? setMins(prevstate => prevstate - 1) : timeleft;
+      setSec(timeleft);
+    };
+    const intervalId = setInterval(innerFunc, 1000);
+    return () => clearInterval(intervalId);
   };
   console.log(miliSec);
 
@@ -72,7 +82,7 @@ export default function App() {
       <div id="timer-label"> session </div>
       <div id="time-left">
         {" "}
-        {sessionLength}:{sec === 0 ? sec + "0" : sec}{" "}
+        {mins}:{sec < 10 ? "0" + sec : sec}{" "}
       </div>
       <button onClick={startStop} id="start_stop">
         Start
